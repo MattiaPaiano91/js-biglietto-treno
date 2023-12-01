2eb1a778beeca8522ecdcfde9b793ca5bbcd8444
MattiaPaiano91 <paianomattia1@gmail.com>
2023-12-01 16:05:41 +0100

    -esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+const kilometriDaPercorrere = prompt('Dimmi quanti kilometri vuoi percorrere');
+
+if (isNaN(kilometriDaPercorrere)){
+    alert("Scrivi i kilometri in numero")
+}
+
+const etàDelPasseggero = prompt('Dimmi quanti hanni hai');
+
+if (isNaN(etàDelPasseggero)){
+    alert("Scrivi l'eta in numero")
+}
+
+let PrezzoBiglietto = (kilometriDaPercorrere * 0.21);
+
+
+
+if (etàDelPasseggero < 18){
+    Prezzo = (PrezzoBiglietto * 0.8).toFixed(2);
+}
+
+else if(etàDelPasseggero > 65){
+    Prezzo = (PrezzoBiglietto * 0.6).toFixed(2);
+}
+
+else{
+     Prezzo = PrezzoBiglietto.toFixed(2);
+ }
+
+document.getElementById('prezzo-biglietto').innerHTML = Prezzo
+
+console.log(Prezzo);
+
